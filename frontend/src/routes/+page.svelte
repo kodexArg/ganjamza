75a850117107deceec4e39b69da60ba4c29c4e9c
Kodex <gcavedal@gmail.com>
2023-08-20 13:10:22 -0300

    3nd working commit

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,49 +1,47 @@
 <script>
-  import { onMount, afterUpdate } from "svelte";
-  import Header from "../components/Header.svelte";
-  import Main from "../components/Main.svelte";
-  import TimeLine from "../components/TimeLine.svelte";
-  import Sidebar from "../components/Sidebar.svelte";
+	import { onMount } from 'svelte';
+	import Header from '../components/Header.svelte';
+	import Main from '../components/Main.svelte';
+	import TimeLine from '../components/TimeLine.svelte';
+	import Sidebar from '../components/Sidebar.svelte';
 
-  let data = {};
-  const API = "https://kittygram-api.vercel.app/";
+	let data = {};
+	const API = 'https://kittygram-api.vercel.app/';
 
-  onMount(async () => {
-    try {
-      const response = await fetch(API);
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      data = await response.json();
-      console.log("data:", data);
-    } catch (error) {
-      console.error("Fetch error:", error.message);
-    }
-  });
-
-
+	onMount(async () => {
+		try {
+			const response = await fetch(API);
+			if (!response.ok) {
+				throw new Error(`HTTP error! Status: ${response.status}`);
+			}
+			data = await response.json();
+			console.log('data:', data);
+		} catch (error) {
+			console.error('Fetch error:', error.message);
+		}
+	});
 </script>
 
 <Header />
 <Main>
-
-  <TimeLine posts={data.posts} />
-  <Sidebar {...data.user} />
+	<TimeLine posts={data.posts} />
+	<Sidebar {...data.user} />
 </Main>
 
 <style>
-  @import url("https://fonts.googleapis.com/css?family=Pacifico&display=swap");
-  @import url("https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap");
+	
+	@import url('https://fonts.googleapis.com/css?family=Pacifico&display=swap');
+	@import url('https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap');
 
-  :global(body) {
-    background-color: #fafafa;
-    color: rgba(38, 38, 38, 0.7);
-    font-family: "Lato", sans-serif;
-    margin: 0;
-    padding: 0;
-  }
-  :global(h1, h2, h3) {
-    margin: 0;
-    padding: 0;
-  }
+	:global(body) {
+		background-color: #fafafa;
+		color: rgba(38, 38, 38, 0.7);
+		font-family: 'Lato', sans-serif;
+		margin: 0;
+		padding: 0;
+	}
+	:global(h1, h2, h3) {
+		margin: 0;
+		padding: 0;
+	}
 </style>
