309dba9a4625a9a25203ba2e771acce0f815e25b
Kodex <gcavedal@gmail.com>
2023-08-20 03:16:23 -0300

    2nd working commit

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { onMount, afterUpdate } from "svelte";
+  import Header from "../components/Header.svelte";
+  import Main from "../components/Main.svelte";
+  import TimeLine from "../components/TimeLine.svelte";
+  import Sidebar from "../components/Sidebar.svelte";
+
+  let data = {};
+  const API = "https://kittygram-api.vercel.app/";
+
+  onMount(async () => {
+    try {
+      const response = await fetch(API);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      data = await response.json();
+      console.log("data:", data);
+    } catch (error) {
+      console.error("Fetch error:", error.message);
+    }
+  });
+
+  afterUpdate(() => {
+    console.log("afterUpdate is called!");
+  });
+</script>
+
+<Header />
+<Main>
+
+  <TimeLine posts={data.posts} />
+  <Sidebar {...data.user} />
+</Main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css?family=Pacifico&display=swap");
+  @import url("https://fonts.googleapis.com/css?family=Lato:300,400,600&display=swap");
+
+  :global(body) {
+    background-color: #fafafa;
+    color: rgba(38, 38, 38, 0.7);
+    font-family: "Lato", sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  :global(h1, h2, h3) {
+    margin: 0;
+    padding: 0;
+  }
+</style>
